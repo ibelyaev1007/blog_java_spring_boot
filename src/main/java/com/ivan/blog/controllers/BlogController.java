ee936debbada5b25bcfa37a54e608264c755a42a
Ivan <ivanbeljaev@mail.ru>
2020-10-04 12:47:26 +0500

    Changed html templates

---
+++ src/main/java/com/ivan/blog/controllers/BlogController.java
@@ -0,0 +1,16 @@
+package com.ivan.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+
+    @GetMapping("/blog")
+    public String blogMain(Model model)
+    {
+        return "blog-main";
+    }
+
+}
